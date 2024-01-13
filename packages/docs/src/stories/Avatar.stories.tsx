bcd35f0c913b20b0082fa9d8427cb30bfeef64f9
Matheus Ferreira <matheusfdantas03@gmail.com>
2024-01-12 22:50:38 -0300

    Controles nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/matheusfd3.png',
     alt: 'Matheus Ferreira',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
